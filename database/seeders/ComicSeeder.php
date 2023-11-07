dbc326054bae10e75de9e2b16bdf4a2178569e5a
Marco Zellini <zellinimarco@gmail.com>
2023-11-07 17:01:32 +0100

    Add Comic Seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,13 +5,22 @@ namespace Database\Seeders;
 use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ComicSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
+
+        $new_comic = new Comic();
+
+        $new_comic->title = $faker->realText(50);
+        $new_comic->price = $faker->randomFloat(2, 0, 9999);
+        $new_comic->description = $faker->realText(300);
+
+        $new_comic->save();
     }
 }
