ce9f1a2ecc0509e88b1fe82b7733f57df5658cff
RiccardoSansone <r.sansone@gmail.com>
2023-11-07 17:02:24 +0100

    add comic seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -13,5 +13,10 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
+        $new_comic = new Comic();
+        $new_comic->description = ' Il Lungo Halloween, ambientato nei primi anni di carriera del Cavaliere Oscuro, ci troveremo di fronte ad una serie di omicidi misteriosi e ad una indagine squisitamente complessa ed interessante.';
+        $new_comic->title = 'Il lungo Halloween';
+        $new_comic->price = 29.99;
+        $new_comic->save();
     }
 }
