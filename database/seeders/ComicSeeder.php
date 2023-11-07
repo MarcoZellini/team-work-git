e4a15332a3fbcc5b3bd6a7785b4f29367c27a2a5
rpsobolewski <rpsobolewski@gmail.com>
2023-11-07 17:01:18 +0100

    comic seeder 4

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -13,5 +13,17 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
+        $new_comic = new Comic();
+        $new_comic->price = '99';
+        $new_comic->title = 'batman';
+
+
+        $new_comic->description = 'molto bello';
+
+
+
+
+
+        $new_comic->save();
     }
 }
